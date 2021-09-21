65ef150c73ec50ddf2d200179e02a5d1fe26b67b
savagecabbage123 <mine268253@gmail.com>
2021-09-20 17:08:47 -0700

    Delete cv.py

--- cv.py
+++
@@ -1,9 +0,0 @@
-import cv2
-print("Version")
-print(cv2.__version__)
-image = cv2.imread("butterfly.jpg")
-gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("cool butterfly", image)
-cv2.imshow("gray butterfly", gray_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
