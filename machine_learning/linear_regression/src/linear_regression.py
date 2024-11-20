0a7c06a3197592dc53ebe8857d8d64dd0584dd76
zach_lawrence <mine268253@gmail.com>
2024-11-19 23:47:56 -0600

    Add files via upload

--- machine_learning/linear_regression/src/linear_regression.py
+++ machine_learning/linear_regression/src/linear_regression.py
@@ -1,1 +1,33 @@
+"""This program will be used to calculate linear regression from scratch.
 
+I will be implementing the calculations for linear regression using a variety
+of different methods which include:
+1. Simple Linear Regression
+2. Ordinary Least Squares
+3. Gradient Descent
+
+More methods may be added above later.
+"""
+
+def simple_linear_regression(x: list, y: list) -> float:
+    """Calculates regression coefficient and constant for linear regression.
+
+    Args:
+        x: List containing independent variable data.
+        y: List containing dependent variable data.
+
+    Returns:
+        m: Linear regression coefficient.
+        b: Linear regression constant.
+    """
+
+def main():
+    """Main function which runs program."""
+    #example input for now
+    xdata = [1.1, 2.2, 3.2, 4.2, 5.2]
+    ydata = [1.1, 2.2, 3.2, 4.2, 5.9]
+
+    simple_linear_regression(xdata, ydata)
+
+if __name__ == '__main__':
+    main()
