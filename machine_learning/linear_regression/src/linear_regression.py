e657ec57cb06b492063679faa0c1be2f8b946ff0
zach_lawrence <mine268253@gmail.com>
2024-11-24 21:30:21 -0600

    updated function variables, and added functions

--- machine_learning/linear_regression/src/linear_regression.py
+++ machine_learning/linear_regression/src/linear_regression.py
@@ -1,3 +1,4 @@
+import math
 """This program will be used to calculate linear regression from scratch.
 
 I will be implementing the calculations for linear regression using a variety
@@ -9,25 +10,115 @@ of different methods which include:
 More methods may be added above later.
 """
 
-def simple_linear_regression(x: list, y: list) -> float:
+def mean_calculator(var_data: list[float]) -> float:
+    """Calculates the mean of a set of data.
+
+    Args:
+        var_data: List containing data regarding a variable.
+
+    returns:
+        The mean of the set of data.
+    """
+    mean: float = 0
+    for val in var_data:
+        mean += val
+    return mean / len(var_data)
+
+def sample_standard_deviation(var_sample_data: list[float], mean: float) -> float:
+    """Calculates the standard deviation of a sample set of data.
+
+    Args:
+        var_sample_data: List containing sample of population data.
+
+    Returns:
+        The sample standard deviation of a dataset.
+    """
+    sample_size = len(var_sample_data) - 1
+    stdev = 0
+    for i in var_sample_data:
+        stdev += var_sample_data[i] - mean
+        stdev = stdev**2
+    stdev = stdev / sample_size
+    stdev = math.sqrt(stdev)
+    return stdev
+
+def population_standard_deviation(var_pop_data: list[float], pop_mean: float) -> float:
+    """Calculates the standard deviation of a full population set of data.
+
+    Args:
+        var_pop_data: List containing full population data.
+
+    Returns:
+        The population standard deviation of a dataset.
+    """
+    sample_size = len(var_pop_data)
+    pop_stdev = 0
+    for i in var_pop_data:
+        pop_stdev += var_pop_data[i] - pop_mean
+        pop_stdev = pop_stdev**2
+    pop_stdev = pop_stdev / sample_size
+    pop_stdev = math.sqrt(pop_stdev)
+    return pop_stdev
+
+def pearsons_correlation_coefficient(independent_var_data: list[float], dependent_var_data: list[float]) -> float:
+    """Calculates the correlation coefficient between 2 variables
+    
+    Args:
+        independent_var_data: List containing independent variable data.
+        dependent_var_data: List containing dependent variable data.
+
+    returns:
+        Correlation coefficient between 2 variables
+    """
+
+def simple_linear_regression(independent_var_data: list[float], dependent_var_data: list[float]) -> float:
     """Calculates regression coefficient and constant for linear regression.
 
     Args:
-        x: List containing independent variable data.
-        y: List containing dependent variable data.
+        independent_var_data: List containing independent variable data.
+        dependent_var_data: List containing dependent variable data.
 
     Returns:
         m: Linear regression coefficient.
         b: Linear regression constant.
     """
+    x_mean = mean_calculator(independent_var_data)
+    y_mean = mean_calculator(dependent_var_data)
+    x_stdev = sample_standard_deviation(independent_var_data)
+    y_stdev = sample_standard_deviation(dependent_var_data)
+    
+    m: float = 0
+    b: float = 0
+    return m, b
+
 
 def main():
     """Main function which runs program."""
-    #example input for now
-    xdata = [1.1, 2.2, 3.2, 4.2, 5.2]
-    ydata = [1.1, 2.2, 3.2, 4.2, 5.9]
+    #TODO: data issue - make a way to import clean data easily into program
 
-    simple_linear_regression(xdata, ydata)
+    #example input data with outputs of different functions for temporary testing
+    x_data: list[float] = [1.1, 2.2, 3.2, 4.2, 5.2]
+    y_data: list[float] = [1.1, 2.2, 3.2, 4.2, 5.9]
+    x_mean:float = 0
+    y_mean:float = 0
+    x_stdev:float = 0
+    y_stdev:float = 0
+
+    x_mean = mean_calculator(x_data)
+    y_mean = mean_calculator(y_data)
+
+    print("x mean:", x_mean)
+    print("y mean:", y_mean)
+    
+    x_stdev = sample_standard_deviation(x_data)
+    y_stdev = sample_standard_deviation(y_data)
+
+    print(x_stdev)
+    print(y_stdev)
+    
+    print(population_standard_deviation(x_data))
+    print(population_standard_deviation(y_data))
+    #simple_linear_regression(xdata, ydata)
 
 if __name__ == '__main__':
     main()
